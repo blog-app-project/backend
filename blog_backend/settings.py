39ac90df45c7f2a1748a7a974281d40b06dc2caa
Alexander Panov <palehou@gmail.com>
2023-11-27 12:43:45 +0300

    build fix: STATICFILES_STORAGE  -> STORAGES

--- blog_backend/settings.py
+++ blog_backend/settings.py
@@ -146,18 +146,15 @@ if not DEBUG:  # Tell Django to copy statics to the `staticfiles` directory
     STATIC_ROOT = BASE_DIR / "staticfiles"
     # Turn on WhiteNoise storage backend that takes care of compressing static files
     # and creating unique names for each version so they can safely be cached forever.
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    STORAGES = {
+        # ...
+        "staticfiles": {
+            "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        },
+    }
 else:
     MEDIA_URL = 'media/'
     MEDIA_ROOT = BASE_DIR / 'media'
-
-# Storage backend to compress files and hash them to unique names
-STORAGES = {
-    # ...
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-    },
-}
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
